44d7c520754ae12f768f728b7c79964ed5603865
shrirajpawar4 <pawarshriraj4@gmail.com>
2022-11-05 22:59:18 +0530

    Lottery contract deployed

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,34 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract Lottery is VRFConsumerBase, Ownable {
 
     uint256 public fee;
     bytes32 public keyHash;
-
     address[] public players;
     uint8 maxPlayers;
-    bool pulic gameStarted;
+    bool public gameStarted;
     uint256 entryFee;
-    uint256 gameId;
+    uint256 public gameId;
+
 
     event GameStarted(uint256 gameId, uint8 maxPlayers, uint256 entryFee);
-    event PlayersJoined(uint256 gameId, address player);
+    event PlayerJoined(uint256 gameId, address player);
     event GameEnded(uint256 gameId, address winner,bytes32 requestId);
 
-
-    constructor(address vrfCoordinator, address linkToken, bytes32 vrfKeyHash, uint256 vrfFee) VRFConsumerBase(vrfCoordinator, linkToken) {
+    constructor(address vrfCoordinator, address linkToken,
+    bytes32 vrfKeyHash, uint256 vrfFee)
+    VRFConsumerBase(vrfCoordinator, linkToken) {
         keyHash = vrfKeyHash;
         fee = vrfFee;
-        gameStarted = false;   
+        gameStarted = false;
     }
 
     function startGame(uint8 _maxPlayers, uint256 _entryFee) public onlyOwner {
-        require(!gameStarted, "Game in ongoing");
+        require(!gameStarted, "Game is currently running");
         delete players;
         maxPlayers = _maxPlayers;
         gameStarted = true;
@@ -38,37 +38,31 @@ contract Lottery is VRFConsumerBase, Ownable {
     }
 
     function joinGame() public payable {
-        require(gameStarted, "Game not started yet");
-        require(msg.value == entryFee, "Minimum Fee required to enter");
-        require(players.length < maxPlayers, "Game is full, try later");
-
+        require(gameStarted, "Game has not been started yet");
+        require(msg.value == entryFee, "Value sent is not equal to entryFee");
+        require(players.length < maxPlayers, "Game is full");
         players.push(msg.sender);
-        emit(gameId, msg.sender);
-
-        if (players.length == maxPlayers) {
+        emit PlayerJoined(gameId, msg.sender);
+        if(players.length == maxPlayers) {
             pickWinner();
         }
     }
 
-    function fullfillRandomness(bytes32 requestId, uint256 randomness) intenal virtual override {
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal virtual override  {
         uint256 winnerIndex = randomness % players.length;
         address winner = players[winnerIndex];
-
         (bool sent,) = winner.call{value: address(this).balance}("");
-        require(sent, "Failed to send ether");
-
-        emit GameEnded(gameId, winner, requestId);
-
+        require(sent, "Failed to send Ether");
+        emit GameEnded(gameId, winner,requestId);
         gameStarted = false;
     }
 
     function pickWinner() private returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK tokens");
-
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK");
+        
         return requestRandomness(keyHash, fee);
     }
 
     receive() external payable {}
-
     fallback() external payable {}
 }
